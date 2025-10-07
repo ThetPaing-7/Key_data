3df33a58d7ba92c0846d5f7ab76919f7949d8ad2
ThetPaing-7 <thatpainlin1999@gmail.com>
2025-10-07 08:23:00 +0000

    Add some styling to gride element

--- jsfile.js
+++ jsfile.js
@@ -1,31 +1,37 @@
-function keydata(){
+function keydata(themeswitch,prompt){
     window.addEventListener("keydown", (e) => {
+
+        // 
         console.log(e.keyCode)
         console.log(e.key)
         console.log(e.code)
         console.log(e.location)
         console.log(e)
     })
+
+    
+    // Light mode and darkmode Changes
+        let darmode = localStorage.getItem('darkmode')
+        const themeSwitch = document.getElementById(themeswitch)
+
+        const enableDarkmode = () =>{
+            document.body.classList.add('darkmode')
+            localStorage.setItem("darkmode","active")
+        }
+
+        const disableDarkmode = () =>{
+            document.body.classList.remove("darkmode")
+            localStorage.setItem("darkmode",null)
+        }
+
+        if(darmode === "active") enableDarkmode()
+
+        themeSwitch.addEventListener("click",()=>{
+            darmode = localStorage.getItem('darkmode')
+            darmode != "active" ? enableDarkmode() : disableDarkmode()
+        })
+
+        
 }
 
-keydata()
-
-let darmode = localStorage.getItem('darkmode')
-const themeSwitch = document.getElementById('theme-switch')
-
-const enableDarkmode = () =>{
-    document.body.classList.add('darkmode')
-    localStorage.setItem("darkmode","active")
-}
-
-const disableDarkmode = () =>{
-    document.body.classList.remove("darkmode")
-    localStorage.setItem("darkmode",null)
-}
-
-if(darmode === "active") enableDarkmode()
-
-themeSwitch.addEventListener("click",()=>{
-    darmode = localStorage.getItem('darkmode')
-    darmode != "active" ? enableDarkmode() : disableDarkmode()
-})
+keydata('theme-switch')
