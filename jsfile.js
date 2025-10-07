49c8fa707cdaf51c53b908fc0488cbfa9dbb8e13
ThetPaing-7 <thatpainlin1999@gmail.com>
2025-10-07 05:47:29 +0000

    Done grid style

--- jsfile.js
+++ jsfile.js
@@ -1,11 +1,31 @@
 function keydata(){
     window.addEventListener("keydown", (e) => {
-        console.log(e)
         console.log(e.keyCode)
         console.log(e.key)
         console.log(e.code)
         console.log(e.location)
+        console.log(e)
     })
 }
 
 keydata()
+
+let darmode = localStorage.getItem('darkmode')
+const themeSwitch = document.getElementById('theme-switch')
+
+const enableDarkmode = () =>{
+    document.body.classList.add('darkmode')
+    localStorage.setItem("darkmode","active")
+}
+
+const disableDarkmode = () =>{
+    document.body.classList.remove("darkmode")
+    localStorage.setItem("darkmode",null)
+}
+
+if(darmode === "active") enableDarkmode()
+
+themeSwitch.addEventListener("click",()=>{
+    darmode = localStorage.getItem('darkmode')
+    darmode != "active" ? enableDarkmode() : disableDarkmode()
+})
